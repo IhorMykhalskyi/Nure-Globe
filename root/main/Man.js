8aade03c5efd42ae69dff7202cfe0d4d21d7f4fb
volodymyr bondariev <vladmikhbond@hotmail.com>
2016-10-28 10:31:46 +0300

    Man

---
+++ root/main/Man.js
@@ -0,0 +1,14 @@
+﻿function Man(x, y, z, points) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    for (var i = 0; i < 2; ++i) {
+        this.imgs[i] = new Image();
+        this.imgs[i].src = 'pic/man' + (i + 1) + '.png';
+    }
+    this.i = 0
+
+
+    
+
+}
