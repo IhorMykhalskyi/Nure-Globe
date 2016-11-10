b96d7b636a85ca1c5909750c9f5604f2c15c04e8
sasha-bogma <sasha.bogma@gmail.com>
2016-11-10 15:07:45 +0200

    Points on the first floor fixed

--- root/tools/MovePoints/MovePoints/MovePoints.cs
+++ root/tools/MovePoints/MovePoints/MovePoints.cs
@@ -18,16 +18,18 @@ namespace MovePoints
 
         private void button1_Click(object sender, EventArgs e)
         {
+            richTextBox2.Text = "";
+
             string text = richTextBox1.Text;
 
-            string pattern = "(\".+\\s\\[)([0-9]+),\\s([0-9]+)(,.+\\])";
+            string pattern = "(\".+\\s?\\[)([0-9]+),\\s?([0-9]+)(,.+\\])";
 
             foreach (Match match in Regex.Matches(text, pattern))
             {
                 int x = Convert.ToInt32(match.Groups[2].Value) + (int)numericUpDown1.Value;
                 int y = Convert.ToInt32(match.Groups[3].Value) + (int)numericUpDown2.Value;
 
-                string line = match.Groups[1].Value + Convert.ToString(x) + ", " + Convert.ToString(y) + match.Groups[4].Value + ",\n"; 
+                string line = match.Groups[1].Value + Convert.ToString(x) + "," + Convert.ToString(y) + match.Groups[4].Value + ",\n"; 
                 richTextBox2.AppendText(line);
             }
         }
--- root/tools/collect.js
+++ root/tools/collect.js
@@ -5,8 +5,8 @@
 // 4) вручную скопировать словарь из текстового поля в файл dots.js  ("var dots = словарь")
 // 5) создать список сегментов (аудитория, перекресток)
 
-var IMG_SRC = "floors/2.svg";
-var Z = 2;
+var IMG_SRC = "floors/1_new.svg";
+var Z = 1;
 
 
 var ctx, canvas;
@@ -201,7 +201,7 @@ function draw() {
     ctx.save();
     ctx.translate(shift_x, shift_y);
     ctx.scale(scale, scale);
-    ctx.drawImage(img, 0, 0);
+    ctx.drawImage(img, 0, 0, 1200, 900);
 
     // dots
     ctx.strokeStyle = "#FF0000"
