9827eb5551d306540ac8420c8cfda2d607553b65
Игорь Михальский <ihor.mykhalskyi@nure.ua>
2016-11-15 20:57:55 +0200

    floors

--- root/tools/collect.js
+++ root/tools/collect.js
@@ -5,8 +5,8 @@
 // 4) вручную скопировать словарь из текстового поля в файл dots.js  ("var dots = словарь")
 // 5) создать список сегментов (аудитория, перекресток)
 
-var IMG_SRC = "floors/2.svg";
-var Z = 2;
+var IMG_SRC = "floors/4_new.svg";
+var Z = 4;
 
 
 var ctx, canvas;
